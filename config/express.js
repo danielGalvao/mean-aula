71733a953bf90bfde3d48cece412feca581fee0d
Daniel Galvão <galvaodev@gmail.com>
2016-09-27 23:16:37 -0300

    Add route to return images

--- config/express.js
+++ config/express.js
@@ -3,4 +3,12 @@ var express = require('express')
 
 app.use(express.static('./public'));
 
+app.get('/v1/fotos', function(req, res){
+  var fotos = [
+    {_id:1, titulo:'Leao', url:'http://i0.wp.com/spacenews.com.br/wp-content/uploads/2015/11/maxresdefault.jpg'},
+    {_id:2, titulo:'Leao 2', url:'http://i0.wp.com/spacenews.com.br/wp-content/uploads/2015/11/maxresdefault.jpg'}
+  ];
+  res.json(fotos);
+});
+
 module.exports = app;
