82636f5e64fea25ba9aa9113ff271d9f0be93cde
Daniel Galvão <galvaodev@gmail.com>
2016-09-27 23:51:44 -0300

    using route folder

---
+++ app/api/foto.js
@@ -0,0 +1,11 @@
+var api = {};
+api.lista = function(req, res){
+
+  var fotos = [
+    {_id:1, titulo:'Leao', url:'http://i0.wp.com/spacenews.com.br/wp-content/uploads/2015/11/maxresdefault.jpg'},
+    {_id:2, titulo:'Leao 2', url:'http://i0.wp.com/spacenews.com.br/wp-content/uploads/2015/11/maxresdefault.jpg'}
+  ];
+  res.json(fotos);
+  
+};
+module.exports = api;
--- app/routes/foto.js
+++ app/routes/foto.js
@@ -1,10 +1,6 @@
+var api = require('../api/foto');
+
 module.exports = function(app){
 
-  app.get('/v1/fotos', function(req, res){
-    var fotos = [
-      {_id:1, titulo:'Leao', url:'http://i0.wp.com/spacenews.com.br/wp-content/uploads/2015/11/maxresdefault.jpg'},
-      {_id:2, titulo:'Leao 2', url:'http://i0.wp.com/spacenews.com.br/wp-content/uploads/2015/11/maxresdefault.jpg'}
-    ];
-    res.json(fotos);
-  });
+  app.get('/v1/fotos', api.lista);
 }
