d4f0ccffb9f7820135d3f7cf6ef3e5b877de1e1c
Daniel Galvão <galvaodev@gmail.com>
2016-09-28 00:03:33 -0300

    using route folder and consign

--- app/api/foto.js
+++ app/api/foto.js
@@ -1,4 +1,5 @@
 var api = {};
+
 api.lista = function(req, res){
 
   var fotos = [
@@ -6,6 +7,6 @@ api.lista = function(req, res){
     {_id:2, titulo:'Leao 2', url:'http://i0.wp.com/spacenews.com.br/wp-content/uploads/2015/11/maxresdefault.jpg'}
   ];
   res.json(fotos);
-  
+
 };
 module.exports = api;
