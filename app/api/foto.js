59ad2e2b21e2e6b0aacbde974a1652804daeb86a
Daniel Galvão <galvaodev@gmail.com>
2016-09-28 22:38:47 -0300

    Add route to get foto by Id

--- app/api/foto.js
+++ app/api/foto.js
@@ -1,12 +1,18 @@
 var api = {};
+var fotos = [
+  {_id:1, titulo:'Leao', url:'http://i0.wp.com/spacenews.com.br/wp-content/uploads/2015/11/maxresdefault.jpg'},
+  {_id:2, titulo:'Leao 2', url:'http://i0.wp.com/spacenews.com.br/wp-content/uploads/2015/11/maxresdefault.jpg'}
+];
 
 api.lista = function(req, res){
-
-  var fotos = [
-    {_id:1, titulo:'Leao', url:'http://i0.wp.com/spacenews.com.br/wp-content/uploads/2015/11/maxresdefault.jpg'},
-    {_id:2, titulo:'Leao 2', url:'http://i0.wp.com/spacenews.com.br/wp-content/uploads/2015/11/maxresdefault.jpg'}
-  ];
   res.json(fotos);
-
 };
+
+api.buscaPorId = function(req, res){
+  var foto = fotos.find(function(foto){
+    return foto._id == req.params.id;
+  });
+  res.json(foto);
+};
+
 module.exports = api;
